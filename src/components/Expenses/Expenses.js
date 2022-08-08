3c50d104af5ac2dd7ac5bf32ff8c0771eadae6be
cibss <cibss13@gmail.com>
2022-08-08 10:03:43 +0700

    lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,35 +6,29 @@ import ExpensesFilter from "../NewExpense/ExpensesFilter";
 import './Expenses.css'
 
 const Expenses = (props) => {
-    const [enteredYear, setEnteredYear] = useState('2022');
+    const [selectedYear, setSelectedYear] = useState('2022');
 
     const filterYearHandler = (year) => {
-        setEnteredYear(year);
+        setSelectedYear(year);
     };
+
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === selectedYear;
+    })
     
     return (
         <Card className="expenses">
-            <ExpensesFilter selected={enteredYear} onFilterYear={filterYearHandler}/>
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
+            <ExpensesFilter selected={selectedYear} onFilterYear={filterYearHandler}/>
+            {
+                filteredExpenses.map((expense) => (
+                    <ExpenseItem 
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))
+            }
         </Card>
     )
 }
